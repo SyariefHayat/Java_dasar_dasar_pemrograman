dfb02cacacfaffb7a55a5a9e6e5ea932c1426340
SyariefHayat <muhammadsyarifhayat@gmail.com>
2024-12-29 10:41:18 +0700

    ADD: 3. Variabel

---
+++ 3. Variabel/src/App.java
@@ -0,0 +1,42 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("========================");
+        System.out.println("        Variabel        ");
+        System.out.println("========================");
+
+        System.out.println("=========================");
+        System.out.println("Format Penulisan Variabel");
+        System.out.println("=========================");
+
+        System.out.println("typeData namaVariable = value;");
+        System.out.println("int bilanganPertama = 10;");
+
+        // inisialisasi
+        int bilanganPertama = 10;
+
+        // deskripsi
+        int bilanganKedua;
+
+        // assignment
+        bilanganKedua = 20;
+
+        System.out.println("===========================");
+        System.out.println("Mencetak Variabel Ke Konsol");
+        System.out.println("===========================");
+
+        // cetak
+        System.out.println("Bilangan Pertama: " + bilanganPertama);
+        System.out.println("Bilangan Kedua: " + bilanganKedua);
+
+        System.out.println("=======================");
+        System.out.println("Mengubah Nilai Variabel");
+        System.out.println("=======================");
+
+        // ubah nilai
+        bilanganPertama = 30;
+        bilanganKedua = 40;
+
+        System.out.println("Bilangan Pertama: " + bilanganPertama);
+        System.out.println("Bilangan Kedua: " + bilanganKedua);
+    }
+}
