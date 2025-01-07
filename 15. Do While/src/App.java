3cad94703dc92e7e35e248fe8a38adba900244e9
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-01-07 22:23:34 +0700

    ADD: 15. Do While

---
+++ 15. Do While/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("================");
+        System.out.println("    Do While    ");
+        System.out.println("================");
+
+        int nilaiAwal = 10;
+
+        do {
+            System.out.println("Loop di jalankan untuk pertama kali: " + nilaiAwal);
+        } while (nilaiAwal > 20);
+    }
+}
