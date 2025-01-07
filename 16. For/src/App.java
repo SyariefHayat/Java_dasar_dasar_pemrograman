948cc0d0a37a11435389a3097dfcf29d2b02b5bc
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-01-07 22:25:56 +0700

    ADD: 16. For

---
+++ 16. For/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("===========");
+        System.out.println("    For    ");
+        System.out.println("===========");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Perulangan ke-" + (i + 1));
+        }
+    }
+}
