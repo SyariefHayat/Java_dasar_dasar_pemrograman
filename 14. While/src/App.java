31ba9fefa2f81a1402cbb6a37da8431fd2744468
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-01-07 22:19:11 +0700

    ADD: 14. While

---
+++ 14. While/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=============");
+        System.out.println("    While    ");
+        System.out.println("=============");
+
+        int nilaiAwal = 1;
+
+        while (nilaiAwal <= 10) {
+            System.out.println( "Loop ke: " + nilaiAwal);
+            nilaiAwal++;
+        }
+    }
+}
